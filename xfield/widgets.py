3d5f81450eb6a959090f6ce1a25bd403e6693591
Phui Hock <phuihock@gmail.com>
2011-06-25 00:38:53 +0800

    pad values to min_values length if the given number of values is less than min_values.
    This will ensure the correct error message is displayed.

--- xfield/widgets.py
+++ xfield/widgets.py
@@ -1,6 +1,6 @@
 from django.utils.datastructures import MultiValueDict, MergeDict
 
-def ExpandableWidgetFactory(base_widget):
+def ExpandableWidgetFactory(base_widget, min_values, max_values):
     class _ExpandableWidget(base_widget):
         def __getattr__(self, name):
             return getattr(base_widget, name)
@@ -8,25 +8,36 @@ def ExpandableWidgetFactory(base_widget):
         def value_from_datadict(self, data, files, name):
             data_dict = getattr(self, 'input_type', None) == 'file' and files or data
             if isinstance(data_dict, (MultiValueDict, MergeDict)):
-                return data_dict.getlist(name)
+                values = data_dict.getlist(name)
             else:
-                return data_dict.get(name)
+                values = data_dict.get(name)
+            return self._pad(values)
+
+        def _pad(self, values):
+            # pad values as necessary
+            if not values:
+                values = [''] * min_values
+            elif len(values) < min_values:
+                values += [''] * (min_values - len(values))
+            return values
 
         def _render_generator_factory(self):
-            def render_generator(name, value, attrs):
+            def render_generator(name, values, attrs):
                 # delete the 'id' key from attrs if it exists because an expandable field should not have id
                 if isinstance(attrs, dict) and 'id' in attrs:
                     del attrs['id']
 
-                if value:
-                    for v in value:  # value is a list
-                        yield super(_ExpandableWidget, self).render(name, v, attrs)
-                raise StopIteration
+                values = self._pad(values)
+                for v in values:  
+                    yield super(_ExpandableWidget, self).render(name, v, attrs)
+                else:
+                    raise StopIteration
             return render_generator
 
-        def render(self, name, value, attrs=None):
+        def render(self, name, values, attrs=None):
             if not hasattr(self, '_render_generator'):
-                self._render_generator = self._render_generator_factory()(name, value, attrs)
+                # values must be an iterator or a list
+                self._render_generator = self._render_generator_factory()(name, values, attrs)
 
             try:
                 return self._render_generator.next()
@@ -36,7 +47,7 @@ def ExpandableWidgetFactory(base_widget):
     return _ExpandableWidget
 
 
-def ExpandableWidget(base_widget, **kwargs):
+def ExpandableWidget(base_widget, min_values=0, max_values=None, **kwargs):
     """
     Given a widget, returns an expandable version of the widget.
     An expandable widget accepts zero or more values in GET or POST data. For example,
@@ -55,4 +66,4 @@ def ExpandableWidget(base_widget, **kwargs):
         def clean_food(self):
             list_of_food = self.cleaned_data['food']  # this is a list of strings
     """
-    return ExpandableWidgetFactory(base_widget)(**kwargs)
+    return ExpandableWidgetFactory(base_widget, min_values, max_values)(**kwargs)
