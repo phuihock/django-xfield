91439bc4b1e6fa432680241e81fd264ae6ffdead
Phui Hock <phuihock@gmail.com>
2011-06-25 17:34:13 +0800

    serve demo with mod_wsgi

--- urls.py
+++ urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', redirect_to, {'url': '/demo/'}),
-    url(r'^demo/', include('demo.urls')),
+    url(r'^', include('demo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
